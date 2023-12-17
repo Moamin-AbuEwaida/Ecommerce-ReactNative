6c1b93b9a4b1704603d335c7228dc885fb0dd5a0
Moamin <moamin.salamah@gmail.com>
2023-12-17 12:28:12 +0100

    add dark mode indication

--- App.js
+++ App.js
@@ -2,15 +2,33 @@ import { StatusBar } from "expo-status-bar";
 import { Switch, Text, View, SafeAreaView } from "react-native";
 import { useColorScheme } from "nativewind";
 import ProductsList from "./components/productsList";
+import { Fontisto, Ionicons } from "@expo/vector-icons";
 export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
-      <View className="flex-row w-full gap-5">
-        <Text className="text-2xl font-bold dark:text-white">
+      <View className="flex-row justify-between w-full gap-5">
+        <Text className="text-2xl pt-2 font-bold dark:text-white">
           New Collection{" "}
         </Text>
-        <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
+        <View className="flex-row gap-2 pb-2 ">
+          {colorScheme === "light" ? (
+            <Fontisto
+              name="day-sunny"
+              size={24}
+              color="black"
+              style={{ paddingVertical: 4 }}
+            />
+          ) : (
+            <Ionicons
+              name="moon"
+              size={24}
+              color="white"
+              style={{ paddingVertical: 4 }}
+            />
+          )}
+          <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
+        </View>
       </View>
       <ProductsList />
       <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
