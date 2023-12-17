baca5f5e89bccc9164fbdba40486f740a55129e3
Moamin <moamin.salamah@gmail.com>
2023-12-17 09:04:22 +0100

    finalizing preparing app.js file

--- App.js
+++ App.js
@@ -7,10 +7,12 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
       <View className="flex-row w-full gap-5">
-        <Text className="dark:text-white">New Collection </Text>
+        <Text className="text-2xl font-bold dark:text-white">
+          New Collection{" "}
+        </Text>
         <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
     </View>
   );
 }
