2d66b96d32d1a81e5711d2ca9ec1f10d0d540d22
Moamin <moamin.salamah@gmail.com>
2023-12-17 09:00:41 +0100

    dark mode toggle setup

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Switch, Text, View } from "react-native";
+import { useColorScheme } from "nativewind";
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <View className="flex-1 bg-sky-400 ">
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-200 dark:bg-black">
+      <View className="flex-row w-full gap-5">
+        <Text className="dark:text-white">New Collection </Text>
+        <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
