c619c6c5080cff26e6e07e2f9d867ce9323e3a3d
Moamin <moamin.salamah@gmail.com>
2023-12-17 10:51:58 +0100

    product card component

--- components/productCard.js
+++ components/productCard.js
@@ -7,10 +7,52 @@ const productCard = ({ image, category, title, price, description }) => {
   const [count, setCount] = useState(1);
   const { colorScheme } = useColorScheme();
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
-      <Text>{price}</Text>
+    <View className="w-full bg-white dark:bg-gray-50/10 rounded-3xl p-5 my-5">
+      <View className="bg-white rounded-xl">
+        <Image
+          source={{ uri: image }}
+          className="w-full h-72"
+          style={{ resizeMode: "contain" }}
+        />
+      </View>
+      <View className="mt-5">
+        <Text className="text-sm uppercase text-black/60 dark:text-white/70">
+          {category}
+        </Text>
+        <Text className="text-lg font-semibold dark:text-white">{title}</Text>
+        <View className="flex-row justify-between my-3">
+          <View className="flex-row items-center gap-3">
+            <AntDesign
+              name="minuscircleo"
+              size={24}
+              color={colorScheme === "light" ? "black" : "white"}
+              onPress={() => setCount(count - 1)}
+            />
+            <Text className="text-xl dark:text-white">{count}</Text>
+            <AntDesign
+              name="pluscircleo"
+              size={24}
+              color={colorScheme === "light" ? "black" : "white"}
+              onPress={() => setCount(count + 1)}
+            />
+          </View>
+          <Text className="text-2xl font-extrabold text-green-500">
+            € {price * count}
+          </Text>
+        </View>
+
+        <Text
+          numberOfLines={2}
+          className="text-sm text-black/60 dark:text-white/70"
+        >
+          {description}
+        </Text>
+      </View>
+      <TouchableOpacity className="flex-row justify-center w-10/12 self-center mt-5 bg-black dark:bg-white p-5 rounded-full">
+        <Text className="text-white dark:text-black font-bold">
+          Add To Cart
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
